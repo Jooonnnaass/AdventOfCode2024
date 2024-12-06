a9b8d876ecf17a661bd488b541c179410611609e
Jooonnnaass <produktiverjonas@gmail.com>
2024-12-06 10:22:01 +0100

    one part one

--- subfolder/examples/one.rs
+++ subfolder/examples/one.rs
@@ -1,33 +1,28 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, i32, io::Read};
 
 fn main() {
-    let mut string: String = String::new();
+    let mut left: Vec<i32> = vec![];
+    let mut right: Vec<i32> = vec![];
+
+    let mut string = String::new();
+
     let _ = File::open("inputs/inputone.txt")
         .expect("Could not open inputone.txt")
         .read_to_string(&mut string);
-    let vec: Vec<i32> = string
-        .split("\n")
-        .map(|n| {
-            if n.contains(" ") {
-                return n.parse::<i32>().unwrap();
-            }
-            0
-        })
-        .collect();
 
-    let mut left_list: Vec<&i32> = Vec::new();
-    let mut right_list: Vec<&i32> = Vec::new();
-
-    for (i, str) in vec.iter().enumerate() {
-        if i % 2 == 0 {
-            left_list.push(str);
-        } else {
-            right_list.push(str);
-        }
+    for line in string.lines() {
+        let mut line_iterator = line.split_whitespace();
+        left.push(line_iterator.next().unwrap().parse::<i32>().unwrap());
+        right.push(line_iterator.next().unwrap().parse::<i32>().unwrap());
     }
 
-    println!("{}", left_list.len());
-    println!("{:?}", left_list);
-    println!("{}", right_list.len());
-    println!("{:?}", right_list);
+    left.sort();
+    right.sort();
+
+    let result: i32 = left
+        .iter()
+        .zip(right)
+        .fold(0, |acc, n| acc + n.0.abs_diff(n.1) as i32);
+
+    println!("{}", result);
 }
